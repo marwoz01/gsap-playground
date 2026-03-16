5498e553046d596d2e6e1a37035248f12eb6c76a
hsu win latt <lareina1467@gmail.com>
2026-03-16 14:11:17 +0700

    chore: change a bit animation

--- pages/27_stack-text-reveal/script.js
+++ pages/27_stack-text-reveal/script.js
@@ -1,1 +1,13 @@
 import gsap from "gsap";
+
+gsap.from(".line span", {
+  yPercent: 150,
+  scaleY: 1.5,
+  skewY: 5,
+  duration: 1.3,
+  ease: "expo.out",
+  stagger: {
+    each: 0.1,
+    from: "end",
+  },
+});
